bbed68ffca37bae75e2048565f2189a9cbccdaeb
Bangley123 <yesasihombing1@gmail.com>
2023-03-19 20:07:43 +0700

    Add project files.

---
+++ tp_modul5_1302213108/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class Program
+{
+    public class HaloGeneric
+    {
+        public static void sapaUser<T>(T inputuser)
+        {
+            Console.WriteLine("Hai user " + inputuser);
+        }
+    }
+    public class DataGeneric<T>
+    {
+        private T data;
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("Data yang disimpan antara lain: " + data);
+        }
+    }
+    private static void Main(string[] args)
+    {
+        HaloGeneric.sapaUser<string>("Yesa Krisanto Sihombing");
+        DataGeneric<string> nim = new DataGeneric<string>("1302213108");
+        nim.PrintData();
+    }
+}
